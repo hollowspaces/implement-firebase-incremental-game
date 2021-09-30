df02b678d1159491a63a2f4eac95699c9e4fef50
Puji <hollowspacesz@gmail.com>
2021-09-30 21:44:03 +0700

    add save/load to cloud, setup, and fire analytics

---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+{
+    public AudioSource tapSfx;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+        tapSfx.Play();
+    }
+}
